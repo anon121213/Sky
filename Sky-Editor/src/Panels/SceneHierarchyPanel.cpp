d3fbbc4090876044137d91da34d3696eee6a2c6d
Arthur <Zheldakovartur@gmail.com>
2026-04-17 22:48:46 +0400

    add inspector panel

--- Sky-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Sky-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -2,6 +2,7 @@
 
 #include <imgui/imgui.h>
 
+#include "glm/gtc/type_ptr.inl"
 #include "Sky/Scene/Components.h"
 
 namespace Sky
@@ -26,6 +27,18 @@ namespace Sky
 			DrawEntityNode(entity);
 		});
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+
+		ImGui::End();
+
+		ImGui::Begin("Inspector");
+
+		if (m_SelectionContext)
+		{
+			DrawComponents(m_SelectionContext);
+		}
+
 		ImGui::End();
 	}
 
@@ -41,5 +54,31 @@ namespace Sky
 
 		if (opened)
 			ImGui::TreePop();
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256] = {};
+			strncpy(buffer, tag.c_str(), sizeof(buffer));
+
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+				ImGui::TreePop();
+			}
+		}
 	}
 }
